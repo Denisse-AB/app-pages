8ff640b77c3f2f6c85a384db2e03b69e443cc99b
Denisse Abreu <denisseabreu21@yahoo.com>
2022-01-30 16:12:48 -0400

    config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,7 @@
 const path = require('path')
-/*
- * const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
- *
- * configureWebpack: {
- *   plugins: [new BundleAnalyzerPlugin()]
- * },
- *
- */
+
 
 module.exports = {
-  // Uncomment outputDir for production.
   publicPath: process.env.NODE_ENV === 'production' ? '/appointments-SPA/' : '/',
   devServer: {
     proxy: {
