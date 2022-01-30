942104cfd4db27da5221fc404fc3cd95a0330e44
Denisse Abreu <denisseabreu21@yahoo.com>
2022-01-30 19:35:10 -0400

    name change

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/appointments-SPA/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/app-pages/' : '/',
   devServer: {
     proxy: {
       '/post': {
