238a5b7be969693c818b4a08f9c703f67329933b
Denisse Abreu <denisseabreu21@yahoo.com>
2022-01-30 16:05:38 -0400

    production build

--- client/vue.config.js
+++ client/vue.config.js
@@ -10,7 +10,7 @@ const path = require('path')
 
 module.exports = {
   // Uncomment outputDir for production.
-  outputDir: path.resolve(__dirname, '../server/public'),
+  publicPath: process.env.NODE_ENV === 'production' ? '/appointments-SPA/' : '/',
   devServer: {
     proxy: {
       '/post': {
